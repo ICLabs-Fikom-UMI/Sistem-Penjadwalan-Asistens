717c1d4e87715a213b859bd0e99c352cbb800d01
imran afdillah <imranafdillah9c@gmail.com>
2024-02-05 23:09:45 +0800

    rubah tampilan form

--- app/views/viewJadwal/index.php
+++ app/views/viewJadwal/index.php
@@ -19,4 +19,37 @@
 <div class="container-btn mt-3 ">
     <a class="btn-logout btn-sm" href="<?= BASEURL?>/login/logout" role="button" onclick="return confirm('Anda Yakin Ingin Keluar')">Logout</a>
 </div>
-<h1>hello</h1>
+<h1>Tampilan Jadwal Kosong Asisten</h1>
+<br><br>
+<table id="example" class="table table-striped" style="width:100%">
+<div class="rayhananjing">
+
+    <thead class="asu">
+        <tr>
+            <th>No</th>
+            <th>Id Jadwal</th>
+            <th>Nama</th>
+            <th>Hari</th>
+            <th>Jam</th>
+            </tr>
+    </thead>
+</div>
+        <tbody>
+          <?php
+          $No=0;
+          foreach($data['form']as $form):
+            $No++;
+            ?>
+            <tr>
+                <td><?=$No;?></td>
+                <td><?=$form['idjadwal'];?></td>
+                <td><?=$form['Nama'];?></td>
+                <td><?=$form['hari'];?></td>
+                <td><?=$form['jam'];?></td>
+                <!-- <td>$320,800</td> -->
+            </tr>
+           <?php
+           endforeach
+           ?>
+           
+        </table> 
